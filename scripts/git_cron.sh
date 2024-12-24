7c3fcef3b73a7b4c75946335981a2157a3247758
UreshiiPanda <jojohoughton22@gmail.com>
2024-12-24 11:42:57 -0600

    auto commit Tue Dec 24 11:42:57 CST 2024

---
+++ scripts/git_cron.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to the repository directories and push changes
+cd ~/dotfiles && git add . && git commit -m "Automated nightly commit" && git push
+cd ~/notes && git add . && git commit -m "Automated nightly commit" && git push
+
+
