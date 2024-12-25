601138a12c87e0824c20ee6707ad6280b6bc9baf
UreshiiPanda <jojohoughton22@gmail.com>
2024-12-25 11:00:01 -0600

    Automated nightly commit

--- scripts/git_cron.sh
+++ scripts/git_cron.sh
@@ -4,4 +4,5 @@
 cd ~/dotfiles && git add . && git commit -m "Automated nightly commit" && git push
 cd ~/notes && git add . && git commit -m "Automated nightly commit" && git push
 
-
+# Brew update
+brew upgrade
